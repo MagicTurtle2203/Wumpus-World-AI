11dd0d249921b2428fd5d9cf68e555fd65e6afe2
Rafferty Chen <raffertychen@gmail.com>
2020-06-23 17:38:02 -0700

    Add some agent actions

--- AI/world_parser.py
+++ AI/world_parser.py
@@ -1,21 +1,16 @@
 from enum import Enum, auto
-from pathlib import Path
 
-import ai
+from action import ActionType
+from ai import AI
 
 
 class Agent:
-    class Direction(Enum):
-        UP = auto()
-        DOWN = auto()
-        LEFT = auto()
-        RIGHT = auto()
-
     def __init__(self):
-        self.agent_X = 0
-        self.agent_Y = 0
+        self.X = 0
+        self.Y = 0
 
-        self.agnet_direction = self.Direction.RIGHT
+        # DIRECTIONS = ["RIGHT", "DOWN", "LEFT", "UP"]
+        self.direction_index = 0    # Decided to use ints rather than an enum to facilitate easier turning
 
         self.has_arrow = True
         self.has_gold = False
@@ -29,10 +24,62 @@ class World:
         PIT = auto()
 
     def __init__(self, filename, debug=False):
+        self.AI = AI()
         self.agent = Agent()
+        self.score = 0
+
+        self.send_breeze = False
+        self.send_bump = False
+        self.send_glitter = False
+        self.send_scream = False
+        self.send_stench = False
 
         with open(filename) as file:
             self.board = self.create_board(file)
+
+    def run(self):
+        while True:
+            action = self.AI.get_action(self.get_senses())  # TODO: write get_senses function
+            self.score -= 1
+
+            if action == ActionType.FORWARD:
+                if self.agent.direction_index == 3:     # UP
+                    if self.agent.Y + 1 > 0:
+                        self.send_bump = True
+                    self.agent.Y = min(self.agent.Y + 1, 3)
+                elif self.agent.direction_index == 1:   # DOWN
+                    if self.agent.Y - 1 < 0:
+                        self.send_bump = True
+                    self.agent.Y = max(self.agent.Y - 1, 0)
+                elif self.agent.direction_index == 2:   # LEFT
+                    if self.agent.X - 1 < 0:
+                        self.send_bump = True
+                    self.agent.X = max(self.agent.X - 1, 0)
+                elif self.agent.direction_index == 0:   # RIGHT
+                    if self.agent.X + 1 > 3:
+                        self.send_bump = True
+                    self.agent.X = min(self.agent.X + 1, 3)
+
+                if self.board[self.agent.X][self.agent.Y] in (self.TileType.WUMPUS, self.TileType.PIT):
+                    self.score -= 1000
+                    break
+            elif action == ActionType.TURNLEFT:
+                self.agent.direction_index = (self.agent.direction_index - 1) % 4
+            elif action == ActionType.TURNRIGHT:
+                self.agent.direction_index = (self.agent.direction_index + 1) % 4
+            elif action == ActionType.GRAB:
+                if self.board[self.agent.X][self.agent.Y] == self.TileType.GOLD:
+                    self.agent.has_gold = True
+                    self.board[self.agent.X][self.agent.Y] == self.TileType.BLANK
+            elif action == ActionType.SHOOT:
+                if self.agent.has_arrow:
+                    pass    # TODO: Check if wumpus is in front of agent. If it is, kill it.
+            elif action == ActionType.CLIMB:
+                if (self.agent.X, self.agent.Y) == (0, 0) and self.agent.has_gold:
+                    self.score += 1000
+                    break
+
+        return self.score
 
     def create_board(self, file: open) -> [[str]]:
         """
