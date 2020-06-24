5d3e1ebb0551d6a70570dfcd8fa97b330fdc36cd
Rafferty Chen <raffertychen@gmail.com>
2020-06-23 22:36:13 -0700

    Add sensors to agent

--- AI/world_parser.py
+++ AI/world_parser.py
@@ -4,29 +4,27 @@ from action import ActionType
 from ai import AI
 
 
-class Agent:
-    DIRECTIONS = ["RIGHT", "DOWN", "LEFT", "UP"]
-
-    def __init__(self):
-        self.X = 0
-        self.Y = 0
-
-        self.direction_index = 0    # Decided to use ints rather than an enum to make turning easier
-
-        self.has_arrow = True
-        self.has_gold = False
-
-
 class World:
+    class Agent:
+        def __init__(self):
+            self.X = 0
+            self.Y = 0
+
+            # DIRECTIONS = ["RIGHT", "DOWN", "LEFT", "UP"]
+            self.direction_index = 0    # Decided to use ints rather than an enum to make turning easier
+
+            self.has_arrow = True
+            self.has_gold = False
+
     class TileType(Enum):
         BLANK = auto()
         GOLD = auto()
         WUMPUS = auto()
         PIT = auto()
 
-    def __init__(self, filename, _AI=AI, debug=False):
-        self.AI = _AI()
-        self.agent = Agent()
+    def __init__(self, filename, _AI=AI(), debug=False):
+        self.AI = _AI
+        self.agent = self.Agent()
         self.score = 0
 
         self.wumpus_X = 0
@@ -42,8 +40,11 @@ class World:
             self.board = self.create_board(file)
 
     def run(self) -> int:
+        """
+        Handles all actions of the AI agent.
+        """
         while True:
-            action = self.AI.get_action(self.get_senses())  # TODO: write get_senses function
+            action = self.AI.get_action(self.get_senses())
             self.score -= 1
 
             if action == ActionType.FORWARD:
@@ -64,7 +65,7 @@ class World:
                         self.send_bump = True
                     self.agent.X = min(self.agent.X + 1, 3)
 
-                if self.board[self.agent.X][self.agent.Y] in (self.TileType.WUMPUS, self.TileType.PIT):
+                if self.board[self.agent.Y][self.agent.X] in (self.TileType.WUMPUS, self.TileType.PIT):
                     self.score -= 1000
                     break
                 # TODO: Check if agent moved into squares adjacent to pit or wumpus and
@@ -75,35 +76,60 @@ class World:
             elif action == ActionType.TURNRIGHT:
                 self.agent.direction_index = (self.agent.direction_index + 1) % 4
             elif action == ActionType.GRAB:
-                if self.board[self.agent.X][self.agent.Y] == self.TileType.GOLD:
+                if self.board[self.agent.Y][self.agent.X] == self.TileType.GOLD:
                     self.agent.has_gold = True
-                    self.board[self.agent.X][self.agent.Y] == self.TileType.BLANK
+                    self.board[self.agent.Y][self.agent.X] == self.TileType.BLANK
             elif action == ActionType.SHOOT:
                 if self.agent.has_arrow:
                     self.agent.has_arrow = False
 
                     if self.agent.direction_index == 3:     # UP
                         if self.agent.X == self.wumpus_X and self.agent.Y < self.wumpus_Y:
-                            self.board[self.wumpus_X][self.wumpus_Y] = self.TileType.BLANK
+                            self.board[self.wumpus_Y][self.wumpus_X] = self.TileType.BLANK
                             self.send_scream = True
                     elif self.agent.direction_index == 1:   # DOWN
                         if self.agent.X == self.wumpus_X and self.agent.Y > self.wumpus_Y:
-                            self.board[self.wumpus_X][self.wumpus_Y] = self.TileType.BLANK
+                            self.board[self.wumpus_Y][self.wumpus_X] = self.TileType.BLANK
                             self.send_scream = True
                     elif self.agent.direction_index == 2:   # LEFT
                         if self.agent.X > self.wumpus_X and self.agent.Y == self.wumpus_Y:
-                            self.board[self.wumpus_X][self.wumpus_Y] = self.TileType.BLANK
+                            self.board[self.wumpus_Y][self.wumpus_X] = self.TileType.BLANK
                             self.send_scream = True
                     elif self.agent.direction_index == 0:   # RIGHT
                         if self.agent.X < self.wumpus_X and self.agent.Y == self.wumpus_Y:
-                            self.board[self.wumpus_X][self.wumpus_Y] = self.TileType.BLANK
+                            self.board[self.wumpus_Y][self.wumpus_X] = self.TileType.BLANK
                             self.send_scream = True
             elif action == ActionType.CLIMB:
                 if (self.agent.X, self.agent.Y) == (0, 0) and self.agent.has_gold:
                     self.score += 1000
                     break
+            elif action == ActionType.LEAVE:
+                self.score -= 1000
+                break
 
         return self.score
+
+    def get_senses(self) -> [bool]:
+        """
+        Returns a list of boolean values based on flags set in the class.
+        The list follows a format of [stench, breeze, glitter, bump, scream], as specified in the
+        book Artificial Intelligence: A Modern Approach by Peter Norvig and Stuart J. Russell.
+        """
+        senses = [
+            True if self.send_stench else False,
+            True if self.send_breeze else False,
+            True if self.send_glitter else False,
+            True if self.send_bump else False,
+            True if self.send_scream else False
+        ]
+
+        self.send_stench = False
+        self.send_breeze = False
+        self.send_glitter = False
+        self.send_bump = False
+        self.send_scream = False
+
+        return senses
 
     def create_board(self, file: open) -> [[str]]:
         """
